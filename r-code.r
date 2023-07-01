a9b7e9029239ba29f2e3c1b219ae0055ca741db9
Manuel Scionti <93007030+mnlscn@users.noreply.github.com>
2023-07-01 17:56:32 +0200

    Update r-code.r

--- r-code.r
+++ r-code.r
@@ -546,7 +546,61 @@ stopCluster(cl)
 
 
 
- ####### TUNING RF #######
+ 
+
+# SOME Summarizing plots:
+
+OverallTDL <- c(LogitOutput$TDL,SVMOutput$TDL,TreeOutput$TDL,BaggingOutput$TDL,BoostingOutput$TDL,RFOutput$TDL,NNetOutput$TDL)
+OverallGINI <- c(LogitOutput$GINI,SVMOutput$GINI,TreeOutput$GINI,BaggingOutput$GINI,BoostingOutput$GINI,RFOutput$GINI,NNetOutput$GINI)
+
+ForGraph <- data.frame(OverallTDL,OverallGINI)
+
+myLeftAxisLabs <- pretty(seq(0, max(ForGraph$OverallTDL), length.out = 10))
+myRightAxisLabs <- pretty(seq(0, max(ForGraph$OverallGINI), length.out = 10))
+
+myLeftAxisAt <- myLeftAxisLabs/max(ForGraph$OverallTDL)
+myRightAxisAt <- myRightAxisLabs/max(ForGraph$OverallGINI)
+
+ForGraph$OverallTDL1 <- ForGraph$OverallTDL/max(ForGraph$OverallTDL)
+ForGraph$OverallGINI1 <- ForGraph$OverallGINI/max(ForGraph$OverallGINI)
+
+op <- par(mar = c(5,4,4,4) + 0.1)
+
+barplot(t(as.matrix(ForGraph[, c("OverallTDL1", "OverallGINI1")])), beside = TRUE, yaxt = "n", names.arg = c("Logit","SVM","Tree","Bagging","Boosting","Random Forest","Neural Network"), ylim=c(0, max(c(myLeftAxisAt, myRightAxisAt))), ylab =	"Top Decile Lift", legend = c("TDL","GINI"), main="Performance of the Machine Learning Algorithms")
+
+axis(2, at = myLeftAxisAt, labels = myLeftAxisLabs)
+
+axis(4, at = myRightAxisAt, labels = myRightAxisLabs)
+
+mtext("GINI Coefficient", side = 4, line = 3, cex = par("cex.lab"))
+
+mtext(c(paste(round(LogitOutput$TimeElapsed,digits=2),"sec"),
+        paste(round(SVMOutput$TimeElapsed,digits=2),"sec"),
+        paste(round(TreeOutput$TimeElapsed,digits=2),"sec"),
+        paste(round(BaggingOutput$TimeElapsed,digits=2),"sec"),
+        paste(round(BoostingOutput$TimeElapsed,digits=2),"sec"),
+        paste(round(RFOutput$TimeElapsed,digits=2),"sec"),
+        paste(round(NNetOutput$TimeElapsed,digits=2),"sec")), side = 1, line = 3, cex = par("cex.lab"), at = c(2,5,8,11,14,17,20))
+mtext(c(paste(round(LogitOutput$PercCorrect,digits=0),"%"),
+        paste(round(SVMOutput$PercCorrect,digits=0),"%"),
+        paste(round(TreeOutput$PercCorrect,digits=0),"%"),
+        paste(round(BaggingOutput$PercCorrect,digits=0),"%"),
+        paste(round(BoostingOutput$PercCorrect,digits=0),"%"),
+        paste(round(RFOutput$PercCorrect,digits=0),"%"),
+        paste(round(NNetOutput$PercCorrect,digits=0),"%")), side = 1, line = 4, cex = par("cex.lab"), at = c(2,5,8,11,14,17,20))
+
+mtext("Calc. time", side = 1, line = 3, cex = par("cex.lab"), at = -.8)
+mtext("% correct", side = 1, line = 4, cex = par("cex.lab"), at = -.8)
+
+
+lift_obj=lift(ch_in_string~predictionBagging+predictionBoosting+predictionTree+predictionNNet+predictionSVM+predictionlogit,data=x.evaluate,class="ch_in")
+
+ggplot(lift_obj)
+
+
+
+
+####### TUNING RF - BEST MODEL #######
 cl <- makeCluster(detectCores())
 registerDoParallel(cl)
 
@@ -607,60 +661,6 @@ RFOutput$PercCorrect <- mean(x.evaluate$correctRF)*100
 RFconfmatrix <- table(x.evaluate$predictionRFClass,x.evaluate$ch_in_string)
 rm(TimeAux)
 stopCluster(cl)
-
-
-
-
-# SOME Summarizing plots:
-
-OverallTDL <- c(LogitOutput$TDL,SVMOutput$TDL,TreeOutput$TDL,BaggingOutput$TDL,BoostingOutput$TDL,RFOutput$TDL,NNetOutput$TDL)
-OverallGINI <- c(LogitOutput$GINI,SVMOutput$GINI,TreeOutput$GINI,BaggingOutput$GINI,BoostingOutput$GINI,RFOutput$GINI,NNetOutput$GINI)
-
-ForGraph <- data.frame(OverallTDL,OverallGINI)
-
-myLeftAxisLabs <- pretty(seq(0, max(ForGraph$OverallTDL), length.out = 10))
-myRightAxisLabs <- pretty(seq(0, max(ForGraph$OverallGINI), length.out = 10))
-
-myLeftAxisAt <- myLeftAxisLabs/max(ForGraph$OverallTDL)
-myRightAxisAt <- myRightAxisLabs/max(ForGraph$OverallGINI)
-
-ForGraph$OverallTDL1 <- ForGraph$OverallTDL/max(ForGraph$OverallTDL)
-ForGraph$OverallGINI1 <- ForGraph$OverallGINI/max(ForGraph$OverallGINI)
-
-op <- par(mar = c(5,4,4,4) + 0.1)
-
-barplot(t(as.matrix(ForGraph[, c("OverallTDL1", "OverallGINI1")])), beside = TRUE, yaxt = "n", names.arg = c("Logit","SVM","Tree","Bagging","Boosting","Random Forest","Neural Network"), ylim=c(0, max(c(myLeftAxisAt, myRightAxisAt))), ylab =	"Top Decile Lift", legend = c("TDL","GINI"), main="Performance of the Machine Learning Algorithms")
-
-axis(2, at = myLeftAxisAt, labels = myLeftAxisLabs)
-
-axis(4, at = myRightAxisAt, labels = myRightAxisLabs)
-
-mtext("GINI Coefficient", side = 4, line = 3, cex = par("cex.lab"))
-
-mtext(c(paste(round(LogitOutput$TimeElapsed,digits=2),"sec"),
-        paste(round(SVMOutput$TimeElapsed,digits=2),"sec"),
-        paste(round(TreeOutput$TimeElapsed,digits=2),"sec"),
-        paste(round(BaggingOutput$TimeElapsed,digits=2),"sec"),
-        paste(round(BoostingOutput$TimeElapsed,digits=2),"sec"),
-        paste(round(RFOutput$TimeElapsed,digits=2),"sec"),
-        paste(round(NNetOutput$TimeElapsed,digits=2),"sec")), side = 1, line = 3, cex = par("cex.lab"), at = c(2,5,8,11,14,17,20))
-mtext(c(paste(round(LogitOutput$PercCorrect,digits=0),"%"),
-        paste(round(SVMOutput$PercCorrect,digits=0),"%"),
-        paste(round(TreeOutput$PercCorrect,digits=0),"%"),
-        paste(round(BaggingOutput$PercCorrect,digits=0),"%"),
-        paste(round(BoostingOutput$PercCorrect,digits=0),"%"),
-        paste(round(RFOutput$PercCorrect,digits=0),"%"),
-        paste(round(NNetOutput$PercCorrect,digits=0),"%")), side = 1, line = 4, cex = par("cex.lab"), at = c(2,5,8,11,14,17,20))
-
-mtext("Calc. time", side = 1, line = 3, cex = par("cex.lab"), at = -.8)
-mtext("% correct", side = 1, line = 4, cex = par("cex.lab"), at = -.8)
-
-
-lift_obj=lift(ch_in_string~predictionBagging+predictionBoosting+predictionTree+predictionNNet+predictionSVM+predictionlogit,data=x.evaluate,class="ch_in")
-
-ggplot(lift_obj)
-
-
 
 
 
